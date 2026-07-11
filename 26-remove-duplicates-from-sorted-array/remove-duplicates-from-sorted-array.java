6beae1e611974808e45141e3d3b3211da5d74666
Supriya Goud <shagamallasupriya@gmail.com>
2026-07-11 22:59:37 +0530

    Time: 1 ms (78.87%) | Memory: 46.5 MB (81.43%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length==0){
+            return 0;
+        }
+        int slow=1;
+        for(int fast=1;fast<nums.length;fast++){
+            if(nums[fast]!=nums[fast-1]){
+                nums[slow]=nums[fast];
+                slow++;
+            }
+        }
+        return slow;
+    }
+}
